cbbe300db0f3a0ab8ac73d03cccf13df3f6adeda
MrGoodBytes <bytes@mrgoodbytes.dev>
2024-08-02 09:59:00 -0500

    Import providers and tests from test-common-faker

---
+++ src/Providers/Image.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bytes\Common\Faker\Providers;
+
+use Faker\Generator;
+use Faker\Provider\Base;
+use Mmo\Faker\PicsumProvider;
+
+/**
+ * @property Generator $generator
+ */
+class Image extends Base
+{
+    /**
+     * Generate the URL that will return a random image.
+     *
+     * Set randomize to false to remove the random GET parameter at the end of the url.
+     *
+     * @example 'http://via.placeholder.com/640x480.png/CCCCCC?text=well+hi+there'
+     *
+     * @param int         $width
+     * @param int         $height
+     * @param string|null $category
+     * @param bool        $randomize
+     * @param string|null $word
+     * @param bool        $gray
+     * @param string      $format
+     *
+     * @return string
+     */
+    public static function imageUrl(
+        $width = 640,
+        $height = 480,
+        $category = null,
+        $randomize = true,
+        $word = null,
+        $gray = false,
+        $format = 'jpg'
+    ) {
+        return PicsumProvider::picsumUrl(width: $width, height: $height, randomize: $randomize, gray: $gray, imageExtension: $format);
+    }
+}
